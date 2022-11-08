99360f2539b21d6846ff1789e8245c0c46eaa490
GustaJLemos <gustavolemosmendes@gmail.com>
2022-11-07 23:11:58 -0300

    Criando arquivo de cores

---
+++ src/global/colors.ts
@@ -0,0 +1,26 @@
+const gray = {
+  gray100: '#F2F2F2',
+  gray200: '#D9D9D9',
+  gray300: '#808080',
+  gray400: '#333333',
+  gray500: '#262626',
+  gray600: '#1A1A1A',
+  gray700: '#0D0D0D'
+}
+
+const purple = {
+  purple: '#8284FA',
+  purpleDark: '#5E60CE'
+}
+
+const blue = {
+  blue: '#4EA8DE',
+  blueDark: '#1E6F9F'
+}
+
+export const COLORS = {
+  ...gray,
+  ...purple,
+  ...blue,
+  danger: '#E25858'
+}
